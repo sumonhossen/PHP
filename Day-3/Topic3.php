8431e2640aba3c850c9a0e0124ff541c3fb245d2
sumonhossen <49872985+sumonhossen@users.noreply.github.com>
2019-05-10 20:12:38 +0600

    complete

--- Day-3/Topic3.php
+++ Day-3/Topic3.php
@@ -5,7 +5,10 @@ $arrayName3= array("Australia"=>"Canberra","Bangladesh"=>"Barishal");
 $arrayName4= array("India"=>"New Delhi","Bangladesh"=>"Khulna");
 $arrayName5= array("Uganda"=>"Kampala","Thailand"=>"Bangkok","Bangladesh"=>"Bogura" );// More then one multiple Array. 
 
-$array = array($arrayName,$arrayName2,$arrayName3,$arrayName4,$arrayName5 ); //$array are 5 in 1 array
+$array = array($arrayName,$arrayName2,$arrayName3,$arrayName4,$arrayName5 ); //$array is variable where was all value of multiple value
+print_r($array);
+echo "<br>";
+
 for ($i=0; $i <sizeof($array) ; $i++) { 
 	echo $array[$i]["Bangladesh"]."<br>"; // Output where was value of Bangladesh
 }
