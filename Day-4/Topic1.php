f9f09c15b286b5b66f7d26e5b08267a33e00403a
sumonhossen <49872985+sumonhossen@users.noreply.github.com>
2019-05-10 20:40:10 +0600

    simple encoded

---
+++ Day-4/Topic1.php
@@ -0,0 +1,5 @@
+<?php
+$arrayName = array("Bangladesh" =>"Dhaka" ,"Bhutan"=>"Thimphu","Australia"=>"Canberra","India"=>"New Delhi","Uganda"=>"Kampala","Thailand"=>"Bangkok" );
+$encoded=json_encode($arrayName);
+echo $encoded;
+?>
